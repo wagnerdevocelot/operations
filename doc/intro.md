45f6b568f4670cf9c2def965c4dc93ca071e4d9e
vapordev <wagner.dev1337@gmail.com>
2025-04-19 19:16:56 -0300

    Adiciona arquivos iniciais do projeto, incluindo .gitignore, LICENSE, README.md, pom.xml, e configurações de build e dependências.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to operations
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/vapordev/operations_test.clj
@@ -0,0 +1,7 @@
+(ns vapordev.operations-test
+  (:require [clojure.test :refer :all]
+            [vapordev.operations :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
