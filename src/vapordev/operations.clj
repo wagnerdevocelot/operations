fec73a1316d7939cafbddafee8152aa5687c8fdf
vapordev <wagner.dev1337@gmail.com>
2025-04-20 19:53:04 -0300

    Atualiza configuração do projeto e implementa handler para o endpoint /operations, incluindo testes para operações Kafka.

--- src/vapordev/operations.clj
+++ src/vapordev/operations.clj
@@ -1,12 +1,57 @@
 (ns vapordev.operations
+  (:require
+   [ring.adapter.jetty :refer [run-jetty]]
+   [ring.util.response :refer [response]]
+   [cheshire.core :as json]
+   [jackdaw.client :as kafka]
+   [jackdaw.serdes :refer [string-serde edn-serde]])
+  (:import [java.util UUID])
   (:gen-class))
 
-(defn greet
-  "Callable entry point to the application."
-  [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
+(def topic-config
+  "Configuração do tópico Kafka"
+  {:topic-name "operations"
+   :partition-count 1
+   :replication-factor 1
+   :key-serde (string-serde)
+   :value-serde (edn-serde)})
+
+(def producer-config
+  "Configuração do produtor Kafka"
+  {"bootstrap.servers" "localhost:9092"
+   "key.serializer" "org.apache.kafka.common.serialization.StringSerializer"
+   "value.serializer" "org.apache.kafka.common.serialization.StringSerializer"})
+
+(defn create-producer
+  "Cria uma instância do produtor Kafka"
+  []
+  (kafka/producer producer-config))
+
+(defn handler
+  "Handler para o endpoint /operations que processa operações recebidas.
+   Adiciona um timestamp a cada operação e envia para o tópico Kafka."
+  [request]
+  (let [body (json/parse-string (slurp (:body request)) true)
+        operations (map #(assoc % :timestamp (System/currentTimeMillis)) body)]
+    (with-open [producer (create-producer)]
+      (doseq [operation operations]
+        (kafka/produce! producer topic-config (str (UUID/randomUUID)) operation)))
+    (response {:message "Operations processed successfully"})))
+
+(defn start-server
+  "Inicia o servidor HTTP na porta especificada."
+  [port]
+  (run-jetty handler {:port port}))
+
+(defn get-env-var
+  "Função wrapper para obter variáveis de ambiente"
+  [name]
+  (System/getenv name))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (greet {:name (first args)}))
+  "Ponto de entrada principal da aplicação.
+   Inicia o servidor na porta definida pela variável de ambiente PORT ou 3000 por padrão."
+  [& _]
+  (let [port (Integer. (or (get-env-var "PORT") 3000))]
+    (println (str "Starting server on port " port))
+    (start-server port)))
--- test/vapordev/operations_test.clj
+++ test/vapordev/operations_test.clj
@@ -1,7 +1,87 @@
 (ns vapordev.operations-test
-  (:require [clojure.test :refer :all]
-            [vapordev.operations :refer :all]))
+  (:require [clojure.test :refer [deftest is testing]]
+            [ring.mock.request :as mock]
+            [vapordev.operations :refer [handler create-producer start-server -main get-env-var]]
+            [cheshire.core :as json]
+            [jackdaw.client :as jackdaw-client]
+            [ring.adapter.jetty :refer [run-jetty]])
+  (:import [clojure.lang IDeref]
+           [java.io Closeable]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
+(deftype 
+  ^{:doc "Mock do produtor Kafka para testes"}
+  MockProducer []
+  IDeref
+  (deref [_] nil)
+  Closeable
+  (close [_] nil))
+
+(defn mock-produce!
+  "Mock da função produce! do Kafka que simula o envio de mensagens"
+  [_ _ _ _]
+  nil)
+
+(deftest post-operations-endpoint-test
+  (testing "POST /operations endpoint"
+    (with-redefs [vapordev.operations/create-producer (fn [] (MockProducer.))
+                  jackdaw-client/produce! mock-produce!]
+      (let [request (-> (mock/request :post "/operations")
+                        (mock/content-type "application/json")
+                        (mock/body (json/generate-string
+                                    [{"operation" "buy"
+                                      "unit-cost" 10.00
+                                      "quantity" 10000}
+                                     {"operation" "sell"
+                                      "unit-cost" 20.00
+                                      "quantity" 5000}])))
+            response (handler request)]
+        (is (= (:status response) 200))
+        (is (= (:body response) {:message "Operations processed successfully"}))))))
+
+(deftest create-producer-test
+  (testing "Criação do produtor Kafka"
+    (with-redefs [jackdaw-client/producer (fn [_] (MockProducer.))]
+      (let [producer (create-producer)]
+        (is (instance? MockProducer producer))))))
+
+(deftest handler-with-empty-request-test
+  (testing "Handler com requisição vazia"
+    (with-redefs [vapordev.operations/create-producer (fn [] (MockProducer.))
+                  jackdaw-client/produce! mock-produce!]
+      (let [request (-> (mock/request :post "/operations")
+                        (mock/content-type "application/json")
+                        (mock/body (json/generate-string [])))
+            response (handler request)]
+        (is (= (:status response) 200))
+        (is (= (:body response) {:message "Operations processed successfully"}))))))
+
+(deftest handler-with-malformed-request-test
+  (testing "Handler com requisição mal formatada"
+    (with-redefs [vapordev.operations/create-producer (fn [] (MockProducer.))
+                  jackdaw-client/produce! mock-produce!
+                  slurp (fn [_] "not json")]
+      (is (thrown? Exception (handler (mock/request :post "/operations")))))))
+
+(deftest start-server-test
+  (testing "Inicialização do servidor HTTP"
+    (with-redefs [run-jetty (fn [_ {:keys [port]}]
+                              (is (= port 3000))
+                              "mock-server")]
+      (is (= (start-server 3000) "mock-server")))))
+
+(deftest main-function-test
+  (testing "Função main com porta padrão"
+    (with-redefs [start-server (fn [port]
+                                (is (= port 3000))
+                                "mock-server")
+                  get-env-var (fn [_] nil)]
+      (is (= (-main) "mock-server"))))
+
+  (testing "Função main com porta personalizada"
+    (with-redefs [start-server (fn [port]
+                                (is (= port 8080))
+                                "mock-server")
+                  get-env-var (fn [env-var]
+                                 (when (= env-var "PORT") "8080"))]
+      (is (= (-main) "mock-server")))))
+
